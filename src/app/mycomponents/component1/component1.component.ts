6832087095860c2068c00116166c9aa56df3136a
1231313 <1231>
2020-09-16 22:04:00 -0400

    work

---
+++ src/app/mycomponents/component1/component1.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-component1',
+  templateUrl: './component1.component.html',
+  styleUrls: ['./component1.component.css']
+})
+export class Component1Component implements OnInit {
+
+  constructor() { 
+ 
+  }
+  Title = 'Hello Mingyans World, & this is by 【interpolation binding】';
+
+  protected isRed = "green";
+  protected i = 0;
+  protected x;
+  dontclick(){
+    this.i++;
+    if (this.i%2 == 0){this.x = "yes"}
+    else{this.x = "no"}
+    alert(this.x)
+  }
+
+  ngOnInit(): void {
+  }
+}
+
+
+
+
+ 
+ 
